4f1fc0d7cfd939295b2020f48f794c969cfcb17d
vanyauhalin <hi@vanyauhalin.me>
2026-02-04 02:49:19 +0400

    write relative paths explicitly

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import config from "@vanyauhalin/eslint-config"
 export default [
 	...config,
 	{
-		files: ["lib/main.ts"],
+		files: ["./lib/main.ts"],
 		rules: {
 			"jsdoc/require-throws-type": "off", // https://github.com/microsoft/rushstack/issues/5270/
 			"typescript/no-invalid-void-type": "off",
@@ -11,7 +11,7 @@ export default [
 		},
 	},
 	{
-		files: ["README.md/*.ts"],
+		files: ["./README.md/*.ts"],
 		rules: {
 			"typescript/no-unused-vars": "off",
 			"unicorn/no-console-spaces": "off",
